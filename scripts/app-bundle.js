8e5408eb9c3971cebeb9b45bb7dd0430029329b4
r-abbott <r-abbott@users.noreply.github.com>
2017-02-13 22:23:19 -0600

    Contacts tuturial

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -1,10 +1,9 @@
-define('app',['exports', './todo'], function (exports, _todo) {
+define('app',['exports'], function (exports) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-  exports.App = undefined;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -15,24 +14,13 @@ define('app',['exports', './todo'], function (exports, _todo) {
   var App = exports.App = function () {
     function App() {
       _classCallCheck(this, App);
-
-      this.heading = 'Todos';
-      this.todos = [];
-      this.todoDescription = '';
     }
 
-    App.prototype.addTodo = function addTodo() {
-      if (this.todoDescription) {
-        this.todos.push(new _todo.Todo(this.todoDescription));
-        this.todoDescription = '';
-      }
-    };
+    App.prototype.configureRouter = function configureRouter(config, router) {
+      config.title = 'Contacts';
+      config.map([{ route: '', moduleId: 'no-selection', title: 'Select' }, { route: 'contacts/:id', moduleId: 'contact-detail', name: 'contacts' }]);
 
-    App.prototype.removeTodo = function removeTodo(todo) {
-      var index = this.todos.indexOf(todo);
-      if (index !== -1) {
-        this.todos.splice(index, 1);
-      }
+      this.router = router;
     };
 
     return App;
@@ -107,6 +95,141 @@ define('todo',["exports"], function (exports) {
         this.done = false;
     };
 });
+define('utility',["exports"], function (exports) {
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.areEqual = areEqual;
+	function areEqual(obj1, obj2) {
+		return Object.keys(obj1).every(function (key) {
+			return obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
+		});
+	};
+});
+define('web-api',['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var latency = 200;
+  var id = 0;
+
+  function getId() {
+    return ++id;
+  }
+
+  var contacts = [{
+    id: getId(),
+    firstName: 'John',
+    lastName: 'Tolkien',
+    email: 'tolkien@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Clive',
+    lastName: 'Lewis',
+    email: 'lewis@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Owen',
+    lastName: 'Barfield',
+    email: 'barfield@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Charles',
+    lastName: 'Williams',
+    email: 'williams@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Roger',
+    lastName: 'Green',
+    email: 'green@inklings.com',
+    phoneNumber: '867-5309'
+  }];
+
+  var WebAPI = exports.WebAPI = function () {
+    function WebAPI() {
+      _classCallCheck(this, WebAPI);
+
+      this.isRequesting = false;
+    }
+
+    WebAPI.prototype.getContactList = function getContactList() {
+      var _this = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var results = contacts.map(function (x) {
+            return {
+              id: x.id,
+              firstName: x.firstName,
+              lastName: x.lastName,
+              email: x.email
+            };
+          });
+          resolve(results);
+          _this.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.getContactDetails = function getContactDetails(id) {
+      var _this2 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var found = contacts.filter(function (x) {
+            return x.id == id;
+          })[0];
+          resolve(JSON.parse(JSON.stringify(found)));
+          _this2.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.saveContact = function saveContact(contact) {
+      var _this3 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var instance = JSON.parse(JSON.stringify(contact));
+          var found = contacts.filter(function (x) {
+            return x.id == contact.id;
+          })[0];
+
+          if (found) {
+            var index = contacts.indexOf(found);
+            contacts[index] = instance;
+          } else {
+            instance.id = getId();
+            contacts.push(instance);
+          }
+
+          _this3.isRequesting = false;
+          resolve(instance);
+        }, latency);
+      });
+    };
+
+    return WebAPI;
+  }();
+});
 define('resources/index',["exports"], function (exports) {
   "use strict";
 
@@ -116,5 +239,68 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${heading}</h1><form submit.trigger=\"addTodo()\"><input type=\"text\" value.bind=\"todoDescription\"> <button type=\"submit\">Add Todo</button></form><ul><li repeat.for=\"todo of todos\"><input type=\"checkbox\" checked.bind=\"todo.done\"> <span css=\"text-decoration: ${todo.done ? 'line-through' : 'none'}\">${todo.description} </span><button click.trigger=\"removeTodo(todo)\">Remove</button></li></ul></template>"; });
+define('no-selection',["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var NoSelection = exports.NoSelection = function NoSelection() {
+        _classCallCheck(this, NoSelection);
+
+        this.message = "Please Select a Contact.";
+    };
+});
+define('contact-list',['exports', './web-api'], function (exports, _webApi) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.ContactList = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var ContactList = exports.ContactList = function () {
+    ContactList.inject = function inject() {
+      return [_webApi.WebAPI];
+    };
+
+    function ContactList(api) {
+      _classCallCheck(this, ContactList);
+
+      this.api = api;
+      this.contacts = [];
+    }
+
+    ContactList.prototype.created = function created() {
+      var _this = this;
+
+      this.api.getContactList().then(function (contacts) {
+        return _this.contacts = contacts;
+      });
+    };
+
+    ContactList.prototype.select = function select(contact) {
+      this.selectedId = contact.id;
+      return true;
+    };
+
+    return ContactList;
+  }();
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><require from=\"./styles.css\"></require><nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\"><div class=\"navbar-header\"><a class=\"navbar-brand\" href=\"#\"><i class=\"fa fa-user\"></i> <span>Contacts</span></a></div></nav><div class=\"container\"><div class=\"row\"><div class=\"col-md-4\">Contact List Placeholder</div><router-view class=\"col-md-8\"></router-view></div></div></template>"; });
+define('text!styles.css', ['module'], function(module) { module.exports = "body { padding-top: 70px; }\n\nsection {\n  margin: 0 20px;\n}\n\na:focus {\n  outline: none;\n}\n\n.navbar-nav li.loader {\n    margin: 12px 24px 0 6px;\n}\n\n.no-selection {\n  margin: 20px;\n}\n\n.contact-list {\n  overflow-y: auto;\n  border: 1px solid #ddd;\n  padding: 10px;\n}\n\n.panel {\n  margin: 20px;\n}\n\n.button-bar {\n  right: 0;\n  left: 0;\n  bottom: 0;\n  border-top: 1px solid #ddd;\n  background: white;\n}\n\n.button-bar > button {\n  float: right;\n  margin: 20px;\n}\n\nli.list-group-item {\n  list-style: none;\n}\n\nli.list-group-item > a {\n  text-decoration: none;\n}\n\nli.list-group-item.active > a {\n  color: white;\n}\n"; });
+define('text!no-selection.html', ['module'], function(module) { module.exports = "<template><div class=\"no-selection text-center\"><h2>${message}</h2></div></template>"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
-import {Todo} from './todo';
-
 export class App {
-  constructor() {
-    this.heading = 'Todos';
-    this.todos = [];
-    this.todoDescription = '';
-  }
+  configureRouter(config, router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
+    ]);
 
-  addTodo(){
-    if(this.todoDescription){
-      this.todos.push(new Todo(this.todoDescription));
-      this.todoDescription = '';
-    }
-  }
-
-  removeTodo(todo){
-    let index = this.todos.indexOf(todo);
-    if(index !== -1){
-      this.todos.splice(index, 1);
-    }
+    this.router = router;
   }
 }
